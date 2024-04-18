b117806ec47027c59bd91a9229c479de2eca18ec
Tomasz Suski <tomasz.suski@orba.co>
2024-04-18 15:43:59 +0200

    altered Person class by adding fields for properties and getters and setters for those fields

--- src/main/java/com/lingarogroup/peopledb/model/Person.java
+++ src/main/java/com/lingarogroup/peopledb/model/Person.java
@@ -3,9 +3,27 @@ package com.lingarogroup.peopledb.model;
 import java.time.ZonedDateTime;
 
 public class Person {
+    private final String firstname;
+    private final String lastName;
+    private final ZonedDateTime dateOfBirth;
     private Long id;
 
     public Person(String firstname, String lastName, ZonedDateTime dateOfBirth) {
+        this.firstname = firstname;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public ZonedDateTime getDateOfBirth() {
+        return dateOfBirth;
     }
 
     public Long getId() {
